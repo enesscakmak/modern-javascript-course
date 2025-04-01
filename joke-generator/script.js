5f135f0bfa490e8279499ef62ca79f66f24b8dbd
Enes Çakmak <cakmakkeness@gmail.com>
2025-04-01 18:24:37 +0300

    add: course version of it with xhr

--- joke-generator/script.js
+++ joke-generator/script.js
@@ -1,13 +1,38 @@
-const joke_button = document.getElementById("joke-btn");
-const joke_text = document.getElementById("joke");
+// const joke_button = document.getElementById("joke-btn");
+// const joke_text = document.getElementById("joke");
 
-joke_button.addEventListener("click", function (e) {
-	fetch("https://api.chucknorris.io/jokes/random")
-		.then((response) => response.json())
-		.then((data) => replace_joke(data))
-		.catch((error) => console.error("Error:", error));
-});
+// joke_button.addEventListener("click", function (e) {
+// 	fetch("https://api.chucknorris.io/jokes/random")
+// 		.then((response) => response.json())
+// 		.then((data) => replace_joke(data))
+// 		.catch((error) => console.error("Error:", error));
+// });
 
-function replace_joke(joke) {
-	joke_text.textContent = joke.value;
-}
+// function replace_joke(joke) {
+// 	joke_text.textContent = joke.value;
+// }
+
+//course version ↓
+
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("joke-btn");
+
+const generateJoke = () => {
+	const xhr = new XMLHttpRequest();
+
+	xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+
+	xhr.onreadystatechange = function () {
+		if (this.readyState === 4) {
+			if (this.status === 200) {
+				jokeEl.innerHTML = JSON.parse(this.responseText).value;
+			} else {
+				jokeEl.innerHTML = "Something went wrong";
+			}
+		}
+	};
+};
+
+jokeBtn.addEventListener("click", generateJoke);
+
+document.addEventListener("DOMContentLoaded", generateJoke);
